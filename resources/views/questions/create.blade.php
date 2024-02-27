c1229d6e47627835bb27d84e3a1f7b526efd3d4e
Anthony <93596000+AnthonyHartog@users.noreply.github.com>
2024-02-27 09:23:14 +0100

    Questions create form
    Questions create form

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -1,3 +1,23 @@
 <x-app-layout>
-
+    <form action="{{route('questions.store')}}" enctype="multipart/form-data" method="POST">
+        @csrf
+        <div class="form-input-questions">
+            <label for="name">Vraag</label>
+            <input type="text" name="name" id="">
+        </div>
+        <h2>Keuzes</h2>
+        <div class="form-input-questions">
+            <input type="radio" name="right_answer" value="1" id="">
+            <input type="text" name="questions[]" id="">
+        </div>
+        <div class="form-input-questions">
+            <input type="radio" name="right_answer" value="2" id="">
+            <input type="text" name="questions[]" id="">
+        </div>
+        <div class="form-input-questions">
+            <input type="radio" name="right_answer" value="3" id="">
+            <input type="text" name="questions[]" id="">
+        </div>
+        <input type="submit" name="" id="">
+    </form>
 </x-app-layout>
