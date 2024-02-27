16eb556f82df97d9d92905153fae1e079a9defbe
MaykeKerssens <93595867+MaykeKerssens@users.noreply.github.com>
2024-02-27 10:19:53 +0100

    Created QuizSeeder

--- database/seeders/QuizSeeder.php
+++ database/seeders/QuizSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Quiz;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class QuizSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Quiz::create([
+            'name' => 'Oefentoets - Blok 1',
+        ])->questions()->attach([1, 2, 3, 10, 11, 4, 9]);
+
+        Quiz::create([
+            'name' => 'Eindtoets - Jaar 1',
+        ])->questions()->attach([2, 3, 5, 6, 8, 9, 10, 12, 14, 15]);
     }
 }
