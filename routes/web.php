3a1b562d70a3803afa2b7f22f1ca027509e10687
YassineChadili <yassinechadili12@outlook.com>
2024-02-27 09:04:52 +0100

    Make quiz page + QuizController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\QuizController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('quiz', QuizController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
