1bed2f9644815ee54d3f59e8102505cdd9f0a211
Jeremy de Leeuw <jeremydeleeuw@gmail.com>
2024-02-29 15:06:09 +0100

    Created main layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    </head>
-    <body class="antialiased">
-        @include('layouts.navigation')
-        <main>
-            <div class="welcome-msg">
-                @if(isset($user))
-                    <h1 style="font-size: 40px">Welkom {{ $user }}</h1>
-                @else
-                    <h1 style="font-size: 40px">Welkom Gebruiker</h1>
-                @endif
-                <a class="btn-overview" href="{{ url('/overview') }}">Naar Overzicht</a>
-            </div>
-        </main>
-        @include('layouts.footer')
-    </body>
-</html>
+<x-app-layout>
+    <div class="flex items-center text-center flex-col m-[150px]">
+        @if(isset($user))
+            <h1 class="text-6xl">Welkom {{ $user }}</h1>
+        @else
+            <h1 class="text-6xl">Welkom Gebruiker</h1>
+        @endif
+        <a class="border border-black rounded-md text-4xl py-3 px-10 my-10 hover:text-white hover:bg-black duration-500" href="{{ url('/overview') }}">Naar Overzicht</a>
+    </div>
+</x-app-layout>
