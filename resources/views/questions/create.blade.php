e85ff02c7ee75dbc3162ba2d707e7bf19b3dcb12
Anthony <93596000+AnthonyHartog@users.noreply.github.com>
2024-02-29 11:59:09 +0100

    Keuze aanmaken en verwijderen
    Keuze aanmaken en verwijderen

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -15,37 +15,45 @@
             <div class="form-input-questions">
                 <label for="name">Vraag</label>
                 <input type="text" name="question" id="">
-                <input type="submit" name="" id="">
+                <input type="hidden" name="type" value="open">
             </div>
             <input type="submit" name="" id="">
         </form>
     </div>
     <div class="multiple-choice-form" style="display: none">
-        <form action="{{route('questions.store')}}" enctype="multipart/form-data" method="POST">
+        <form action="{{route('questions.store')}}" enctype="multipart/form-data" method="POST" id="extra-questions-container">
             @csrf
-            <div class="form-input-questions">
-                <label for="name">Vraag</label>
-                <input type="text" name="question" id="">
+            <div class="choices">
+                <div class="form-input-questions">
+                    <label for="name">Vraag</label>
+                    <input type="text" name="question" id="">
+                </div>
+                <h2>Keuzes</h2>
+                <div class="form-input-questions-0">
+                    <input type="radio" name="right_answer" value="0" id="">
+                    <input type="text" name="questions[]" id="">
+                </div>
+                <div class="form-input-questions-1">
+                    <input type="radio" name="right_answer" value="1" id="">
+                    <input type="text" name="questions[]" id="">
+                </div>
+                <div class="form-input-questions-2">
+                    <input type="radio" name="right_answer" value="2" id="">
+                    <input type="text" name="questions[]" id="">
+                </div>
             </div>
-            <h2>Keuzes</h2>
-            <div class="form-input-questions">
-                <input type="radio" name="right_answer" value="1" id="">
-                <input type="text" name="questions[]" id="">
+            <div class="addChoices">
+                <button type="button" onclick="addQuestion()">Voeg nog een vraag toe</button>
+                <button type="button" onclick="removeQuestion()">Verwijder een vraag</button>
             </div>
-            <div class="form-input-questions">
-                <input type="radio" name="right_answer" value="2" id="">
-                <input type="text" name="questions[]" id="">
-            </div>
-            <div class="form-input-questions">
-                <input type="radio" name="right_answer" value="3" id="">
-                <input type="text" name="questions[]" id="">
-            </div>
+            <input type="hidden" name="type" value="multiple">
             <input type="submit" name="" id="">
         </form>
     </div>
     <script>
         const openForm = document.querySelector('.open-form');
         const multipleForm = document.querySelector('.multiple-choice-form');
+        let questionNumber = 2;
 
         const open = document.getElementById('open');
         const multiple = document.getElementById('choice');
@@ -58,6 +66,27 @@
             else if(open.checked){
                 openForm.style.display = "block";
                 multipleForm.style.display = "none";
+            }
+        }
+
+        function addQuestion() {
+            let container = document.querySelector('.choices');
+            questionNumber = questionNumber + 1;
+            let div = document.createElement('div');
+            div.className = `form-input-questions-${questionNumber}`;
+            div.innerHTML = `
+                <input type="radio" name="right_answer" value="${questionNumber}" id="">
+                <input type="text" name="questions[]">
+            `;
+            container.appendChild(div);
+        }
+
+        function removeQuestion(){
+            if(questionNumber > 1){
+                let container = document.querySelector(`.form-input-questions-${questionNumber}`);
+                console.log(container);
+                container.parentNode.removeChild(container);
+                questionNumber--;
             }
         }
     </script>
