8b1325b8e37f8b7f2e6a3f58719021e3185dad85
Anthony <93596000+AnthonyHartog@users.noreply.github.com>
2024-02-29 13:24:13 +0100

    front-end tags create
    front-end tags create

--- resources/views/tags/create.blade.php
+++ resources/views/tags/create.blade.php
@@ -1,3 +1,10 @@
 <x-app-layout>
-    <p>Test</p>
+    <form method="POST" action="{{route('tags.store')}}" enctype="multipart/form-data">
+        @csrf
+        <div class="form-input-tags">
+            <label for="name">Naam</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <input type="submit" name="" id="">
+    </form>
 </x-app-layout>
