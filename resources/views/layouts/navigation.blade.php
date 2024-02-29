0bc00bb1edbc6363773ab5eb8b7ba9e86a5700da
Jeremy de Leeuw <jeremydeleeuw@gmail.com>
2024-02-29 15:23:41 +0100

    Updated dashboard url of overzicht button

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,7 +2,7 @@
     <div class="border border-gray-400 bg-gray-400 p-6 flex justify-around">
         <a href="{{ url('/') }}"><img src="{{ asset('img/logo.png') }}" width="200" alt="logo"></a>
         <div class="flex justify-between items-center flex-row">
-            <a class="mx-5 text-3xl" href="{{ url('/overview') }}">Overzicht</a>
+            <a class="mx-5 text-3xl" href="{{ url('/dashboard') }}">Overzicht</a>
             @auth
                 <a class="mx-5 text-3xl" href="{{ url('/questions') }}">Vragen</a>
                 <a class="mx-5 text-3xl" href="{{ url('/tags') }}">Tags</a>
