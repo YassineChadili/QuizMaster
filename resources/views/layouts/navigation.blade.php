e6e4d857fb2582b33cf0943e4f0e8946a9f9f9bb
Jeremy de Leeuw <jeremydeleeuw@gmail.com>
2024-02-29 15:05:50 +0100

    Added tailwind to header

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,10 +1,12 @@
 <header>
-    <div class="nav">
+    <div class="border border-gray-400 bg-gray-400 p-6 flex justify-around">
         <a href="{{ url('/') }}"><img src="{{ asset('img/logo.png') }}" width="200" alt="logo"></a>
-        <div class="nav-links">
-            <a href="{{ url('/overview') }}">Overzicht</a>
-            <a href="{{ url('/questions') }}">Vragen</a>
-            <a href="{{ url('/tags') }}">Tags</a>
+        <div class="flex justify-between items-center flex-row">
+            <a class="mx-5 text-3xl" href="{{ url('/overview') }}">Overzicht</a>
+            @auth
+                <a class="mx-5 text-3xl" href="{{ url('/questions') }}">Vragen</a>
+                <a class="mx-5 text-3xl" href="{{ url('/tags') }}">Tags</a>
+            @endauth
         </div>
         <div class="user">
             @if (Route::has('login'))
