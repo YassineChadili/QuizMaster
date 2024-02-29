fd2d2f56b57bbfead73e562e01e1433798ef1ebc
Jeremy de Leeuw <jeremydeleeuw@gmail.com>
2024-02-29 15:05:33 +0100

    Added tailwind to footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,5 @@
 <footer>
-    <p>&copy; QuizMaster | 2024</p>
+    <div class="border border-gray-400 bg-gray-400 p-6 flex justify-between">
+        <p class="text-xs">&copy; QuizMaster | 2024</p>
+    </div>
 </footer>
