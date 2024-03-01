7ada4025b88f102598d35e9715004207cf2a9729
Anthony <93596000+AnthonyHartog@users.noreply.github.com>
2024-03-01 09:57:47 +0100

    tailwind css tags dashboard
    Normale css naar tailwind css omgezet

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
-    <div class="container" style="display: flex; justify-content: center;">
+    <div class="container flex justify-center">
         <div class="tags">
-            <a href="{{route('tags.create')}}" style="margin-bottom: 15px;">Maak een tag aan</a>
+            <a href="{{route('tags.create')}}" class="mb-15">Maak een tag aan</a>
             @if (\Session::has('Succes'))
                 <p style="color: green">{!! \Session::get('error') !!}</p>
             @endif
@@ -11,13 +11,13 @@
                 @csrf
                 @method('PUT')
                 <input type="text" value="{{$tag->name}}" name="name" id="">
-                <div class="buttons" style="display: flex">
+                <div class="flex">
                     <input type="submit" value="Aanpassen" name="" id="">
             </form>
             <form method="POST" action="{{ route('tags.destroy', $tag->id) }}">
                 @csrf
                 @method('DELETE')
-                <input style="margin-left: 10px" type="submit" value="Verwijderen" name="" id="">
+                <input class="ml-3" type="submit" value="Verwijderen" name="" id="">
                 </div>
             </form>
             @endforeach
