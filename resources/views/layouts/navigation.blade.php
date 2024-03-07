c6c76274d590c900a440b8ca7b122b9caf6cbb24
Anthony <93596000+AnthonyHartog@users.noreply.github.com>
2024-03-07 13:50:27 +0100

    Navigation updated
    Overzicht naar Toetsen gezet.
    url() naar route() gezet

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,10 +2,10 @@
     <div class="border border-gray-400 bg-gray-400 p-6 flex justify-around">
         <a href="{{ url('/') }}"><img src="{{ asset('img/logo.png') }}" width="200" alt="logo"></a>
         <div class="flex justify-between items-center flex-row">
-            <a class="mx-5 text-3xl" href="{{ url('/dashboard') }}">Overzicht</a>
             @auth
-                <a class="mx-5 text-3xl" href="{{ url('/questions') }}">Vragen</a>
-                <a class="mx-5 text-3xl" href="{{ url('/tags') }}">Tags</a>
+            <a class="mx-5 text-3xl" href="{{ route('quiz.index') }}">Toetsen</a>
+                <a class="mx-5 text-3xl" href="{{ route('questions.index') }}">Vragen</a>
+                <a class="mx-5 text-3xl" href="{{ route('tags.index') }}">Tags</a>
             @endauth
         </div>
         <div class="flex items-center">
